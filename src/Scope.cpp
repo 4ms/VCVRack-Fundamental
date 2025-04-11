03232240ae4ff4cd8f4a08ca36ca66d5ae16ae7b
Dan Green <danngreen1@gmail.com>
2025-04-11 11:06:30 -0700

    Scope: do not display "T" on trig line, default ch 2 color to cyan.
    The "T" is not legible on the MM screen

--- src/Scope.cpp
+++ src/Scope.cpp
@@ -376,6 +376,8 @@ struct ScopeDisplay : LedDisplay {
 		}
 		nvgFill(args.vg);
 
+#if !defined(METAMODULE)
+		// This is almost illegible and text that moves is memory-intensive on the MM
 		std::shared_ptr<Font> font = APP->window->loadFont(fontPath);
 		if (font) {
 			nvgFontSize(args.vg, 9);
@@ -383,6 +385,7 @@ struct ScopeDisplay : LedDisplay {
 			nvgFillColor(args.vg, nvgRGBA(0x1e, 0x28, 0x2b, 0xff));
 			nvgText(args.vg, p.x - 8, p.y + 3, "T", NULL);
 		}
+#endif
 		nvgResetScissor(args.vg);
 	}
 
@@ -451,7 +454,7 @@ struct ScopeDisplay : LedDisplay {
 		CableWidget* inputXCable = APP->scene->rack->getTopCable(inputX);
 		CableWidget* inputYCable = APP->scene->rack->getTopCable(inputY);
 		NVGcolor inputXColor = inputXCable ? inputXCable->color : SCHEME_YELLOW;
-		NVGcolor inputYColor = inputYCable ? inputYCable->color : SCHEME_YELLOW;
+		NVGcolor inputYColor = inputYCable ? inputYCable->color : SCHEME_CYAN;
 
 		// Draw waveforms
 		int channelsY = module ? module->channelsY : 1;
